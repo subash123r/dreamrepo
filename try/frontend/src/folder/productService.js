833bee4b6c3c847e616986b638b098e807d0ed79
subash123r <rssubash96@gmail.com>
2026-09-11 12:24:16 +0530

    chamges

--- try/frontend/src/folder/productService.js
+++ try/frontend/src/folder/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/products";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/products`;
 
 // GET ALL
 export const getProducts = async () => {
